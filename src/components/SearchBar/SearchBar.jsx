ad8888bdc8349e244a8fab55c42060306908ff3b
Nikita Mashtakov <n-mashtakov@yandex.ru>
2025-06-05 08:54:26 +0700

    last changes

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,37 +1,49 @@
 import styles from './SearchBar.module.css';
-import { useDispatch } from 'react-redux';
-import { useCallback, useMemo, useState } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { Component, useCallback, useMemo, useState } from 'react';
 import debounce from 'utils/debounce';
 
-export const SearchBar = () => {
-  const [value, setValue] = useState('');
-  const dispatch = useDispatch();
+class SearchBarContainer extends Component {
+  state = { value: '' };
+  // memoizedDispatch = null;
 
-  const memoizedDispatch = useCallback(
-    (value) => dispatch({ type: 'SET_SEARCH', payload: value }),
-    [dispatch],
-  );
+  // const [value, setValue] = useState('');
+  // const dispatch = useDispatch();
+  debouncedSearch = null;
+  // componentDidMount() {
+  memoizedDispatch(value) {
+    this.props.dispatch({ type: 'SET_SEARCH', payload: value });
+    console.log('mount');
+  }
+  deb = (value) => {
+    debounce(this.memoizedDispatch(value), 2500);
+  };
+  // getSnapshotBeforeUpdate() {
+  //   this.debouncedSearch = (value) => debounce(this.memoizedDispatch(value), 250);
+  // }
 
-  const debouncedSearch = useMemo(
-    () => debounce(memoizedDispatch, 250),
-    [memoizedDispatch],
-  );
-
-  const searchHandler = (value) => {
-    setValue(value);
-    debouncedSearch(value);
+  searchHandler = (value) => {
+    console.log('handler');
+    console.log(this.memoizedDispatch);
+    this.setState({ value });
+    console.log(this.debouncedSearch);
+    this.deb(this.state.value);
   };
 
-  return (
-    <div className={styles.container}>
-      <input
-        className={styles.input}
-        type="text"
-        name="search"
-        value={value}
-        placeholder="Поиск..."
-        onChange={({ target }) => searchHandler(target.value)}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className={styles.container}>
+        <input
+          className={styles.input}
+          type="text"
+          name="search"
+          value={this.state.value}
+          placeholder="Поиск..."
+          onChange={({ target }) => this.searchHandler(target.value)}
+        />
+      </div>
+    );
+  }
+}
+
+export const SearchBar = connect()(SearchBarContainer);
